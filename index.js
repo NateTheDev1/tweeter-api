95ccf055a4c43c00632f0b6654acdcfc4afbe565
NateTheDev1 <nthnlrichards@gmail.com>
2020-06-30 02:18:03 -0400

    Server Init

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+
+const cors = require("express");
+app.use(cors());
+
+const dotenv = require("dotenv");
+dotenv.config();
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
+
+const mongoose = require("mongoose");
+mongoose.connect(
+  process.env.DB_CONNECT,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  },
+  () => {
+    console.log("Db Connected");
+  }
+);
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log("Listening on port" + port);
+});
