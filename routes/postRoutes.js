c0dc7c63758d0c7d2870a36805357ff0beb9dc85
NateTheDev1 <nthnlrichards@gmail.com>
2020-07-09 13:49:01 -0400

    New comment routes

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const Post = require("../models/Post");
 const Profile = require("../models/Profile");
+const Comment = require("../models/Comment");
 
 router.get("/all", async (req, res) => {
   const allPosts = await Post.find();
@@ -93,12 +94,31 @@ router.get("/:profileId", async (req, res) => {
 router.delete("/:postId", async (req, res) => {
   try {
     const foundPost = await Post.deleteOne({ _id: req.params.postId });
-    if (foundPost.deletedCount === 0) {
-      return res.json(
-        "No posts were deleted, the specified post does not exist."
-      );
-    }
+
     res.json(foundPost);
+  } catch (err) {
+    res.status(400).send(err);
+  }
+});
+
+router.get("/:postId/comments", async (req, res) => {
+  const comments = await Comment.find({ postId: req.params.postId });
+  const sorted = await comments
+    .slice()
+    .sort((a, b) => b.createdAt - a.createdAt);
+  res.status(200).json(sorted);
+});
+
+router.post("/comments", async (req, res) => {
+  const newComment = new Comment({
+    postedBy: req.body.postedBy,
+    content: req.body.content,
+    postId: req.body.postId,
+  });
+
+  try {
+    const savedComment = await newComment.save();
+    res.status(200).send(savedComment._doc);
   } catch (err) {
     res.status(400).send(err);
   }
